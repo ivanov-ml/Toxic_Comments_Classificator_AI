5d21d1555e581c9dfaaebf36d79433863184541b
Dmitrii.Ivanov <ivanovda15112004@gmail.com>
2026-06-02 16:51:27 +0300

    1st baseline model(MultiOutputClassifier and LogisticRegression)

--- data_prepare.py
+++ data_prepare.py
@@ -34,5 +34,7 @@ print(row_data['lenght_symbols'][0])
 print(row_data['tokens'][0])
 print(row_data['lenght_words'][0])
 print(row_data['comment_text'][0])
+# Удаляем строки, где comment_text == NaN
+row_data = row_data.dropna(subset=['comment_text'])
 
 row_data.to_csv('data/cleaned_data.csv')
