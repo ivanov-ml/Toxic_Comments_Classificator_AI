ea6ab5ead85bb50d97761f84831af576896f99a2
Dmitrii.Ivanov <ivanovda15112004@gmail.com>
2026-06-03 15:39:46 +0300

    Added Pipeline in baseline model

--- BaseLine.py
+++ BaseLine.py
@@ -1,13 +1,13 @@
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.multioutput import MultiOutputClassifier
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import roc_auc_score
+from sklearn.pipeline import Pipeline
 from sklearn.model_selection import train_test_split
 import pandas as pd
-import numpy as np
+from sklearn.metrics import roc_auc_score
+
 
 df = pd.read_csv('data/cleaned_data.csv')
-
 df = df.dropna(subset=['comment_text'])
 
 label_cols = ['toxic', 'severe_toxic', 'obscene', 'threat', 'insult', 'identity_hate']
@@ -16,18 +16,21 @@ y = df[label_cols]
 
 X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2, random_state=42, shuffle=True)
 
-vectorizer = TfidfVectorizer(
-    max_features=10000,
-    ngram_range=(1, 2),
-    stop_words='english'
-)
-X_train_vec = vectorizer.fit_transform(X_train)
-X_val_vec = vectorizer.transform(X_val)
+# Pipeline: векторизация → классификация
+pipeline = Pipeline([
+    ('tfidf', TfidfVectorizer(
+        max_features=10000,
+        ngram_range=(1, 2),
+        stop_words='english'
+    )),
+    ('clf', MultiOutputClassifier(LogisticRegression(class_weight='balanced')))
+])
 
-model = MultiOutputClassifier(LogisticRegression(class_weight='balanced'))
-model.fit(X_train_vec, y_train)
 
-y_pred = model.predict_proba(X_val_vec)
+pipeline.fit(X_train, y_train)
+
+
+y_pred = pipeline.predict_proba(X_val)
 
 
 auc_scores = []
@@ -37,5 +40,3 @@ for i, col in enumerate(label_cols):
     auc_scores.append(auc)
 
 print(f"Mean ROC-AUC: {sum(auc_scores) / len(auc_scores):.4f}")
-
-
